99ae211ef73e000a4cfd251e2d61265f77c9682a
sijanthapa171 <thapasijan329@gmail.com>
2025-08-11 20:43:50 +0545

    home: dro[ branch from git config

--- home-manager/apps/git.nix
+++ home-manager/apps/git.nix
@@ -3,10 +3,9 @@
 {
   home.packages = [ pkgs.git ];
   programs.git.enable = true;
-  programs.git.userName = "sijanthapa171";
-  programs.git.userEmail = "thapasijan329@gmail.com";
+  programs.git.userName = "Nikita Zheleztsov";
+  programs.git.userEmail = "n.zheleztsov@proton.me";
   programs.git.extraConfig = {
-    init.defaultBranch = "main";
     core.editor = "nvim";
   };
 }
